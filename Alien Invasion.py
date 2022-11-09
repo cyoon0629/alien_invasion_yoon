3b172b7df7393371bbb4967ad62b00eed840d26d
cyoon0629 <m256996@usna.edu>
2022-11-08 22:50:47 -0500

    game

---
+++ Alien Invasion.py
@@ -0,0 +1,21 @@
+WIDTH = 600
+HEIGHT = 600
+
+import pygame
+
+#ship = Actor('ship')
+#ship.pos = 300,300
+#center_right = ship.right
+
+def draw():
+    screen.clear()
+    screen.fill((255,255,255))
+    #ship.draw()
+
+
+# Main
+
+pygame.init()
+
+draw()
+
